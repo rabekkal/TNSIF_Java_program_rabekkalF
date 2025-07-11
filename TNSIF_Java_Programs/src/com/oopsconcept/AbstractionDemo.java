f2238260bb2a9b6e37c67eb54b7d2a10c5d0c7dd
kumar <kumar@DESKTOP-VQ8FSOP>
2025-07-11 10:25:22 +0530

    Initial ommit

---
+++ TNSIF_Java_Programs/src/com/oopsconcept/AbstractionDemo.java
@@ -0,0 +1,14 @@
+package com.oopsconcept;
+
+public class AbstractionDemo {
+
+	public AbstractionDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
