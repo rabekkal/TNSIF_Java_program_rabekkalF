487e3c231893621c4c89f51689dac01d18b02382
kumar <kumar@DESKTOP-VQ8FSOP>
2025-07-11 15:00:43 +0530

    commit

--- TNSIF_Java_Programs/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
+++ TNSIF_Java_Programs/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -1,25 +1,10 @@
 package com.tnsif.nonaccessmodifiers;
 
-class FinalDemo {
-	
-	int Max = 100;
-	
-	void display() {
-		System.out.println("show the value of max: "+Max);
-	}
+public class FinalDemo {
 
 	public static void main(String[] args) {
-		FinalDemo fd = new FinalDemo();
-		fd.display();
-		fd.Max = 200;
+		// TODO Auto-generated method stub
 
 	}
 
 }
-
-class Child extends FinalDemo{
-	void display() {
-		System.out.println("show the value of max: "+Max);
-	}
-}
-
